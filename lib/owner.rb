9812243e83085792c6bee6e2123921662bf78df5
Hoon Lee <yl.hoony@gmail.com>
2017-12-12 17:16:51 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -29,9 +29,6 @@ class Owner
   def pets
     self.pets, fishes, dogs, cats = Hash.new
     self.pets.fishes, self.pets.dogs, self.pets.cats = []
-    # self.pets.fishes = []
-    # self.pets.dogs = []
-    # self.pets.cats = []
     self.pets
   end
 
